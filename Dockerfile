d444408b33b5c3dc36ea941f78acb014d7759ad1
Lindoélio Lázaro <lindoelio@gmail.com>
2018-02-20 08:39:58 -0300

    Building only IRCd hybrid

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM ubuntu:16.04
 
-MAINTAINER Lindoélio Lázaro <lindoelio@gmail.com>
+LABEL description="Provides an IRC Network for Rocket.Chat federation"
+LABEL maintainer="lindoelio@gmail.com"
+LABEL version="1.0"
 
 COPY anope-config.cache /tmp
 COPY anope-services.conf /tmp
 COPY hybrid-ircd.conf /tmp
 
-RUN ./bootstrap.sh
+ADD bootstrap.sh /root/bootstrap.sh
+CMD /root/bootstrap.sh
 
 EXPOSE 6667 7000
-ENTRYPOINT ["server", "-b", "0.0.0.0"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Installing pre-requisites
 
-apt update
-apt install -y build-essential cmake openssl libssl-dev wget
+apt-get update
+apt-get install -y build-essential cmake openssl libssl-dev wget
 
 
 # Installing Hybrid (IRC Network Server)
@@ -21,15 +21,15 @@ make && make install
 
 # Installing Anope Services
 
-cd ~ && wget https://github.com/anope/anope/releases/download/2.0.2/anope-2.0.2-source.tar.gz
-
-tar -xzf anope-2.0.2-source.tar.gz && rm anope-2.0.2-source.tar.gz
-
-cp /tmp/anope-config.cache anope-2.0.2-source/config.cache
-
-cd anope-2.0.2-source
-./Config -quick && cd build
-make && make install
+#cd ~ && wget https://github.com/anope/anope/releases/download/2.0.2/anope-2.0.2-source.tar.gz
+#
+#tar -xzf anope-2.0.2-source.tar.gz && rm anope-2.0.2-source.tar.gz
+#
+#cp /tmp/anope-config.cache anope-2.0.2-source/config.cache
+#
+#cd anope-2.0.2-source
+#./Config -quick && cd build
+#make && make install
 
 
 # Configuring pre-requisites
@@ -38,7 +38,7 @@ adduser irc-admin --system --no-create-home
 usermod -a -G sudo irc-admin
 
 chown -R irc-admin /opt/hybrid
-chown -R irc-admin /opt/anope
+#chown -R irc-admin /opt/anope
 
 
 # Configuring and starting Hybrid
@@ -51,7 +51,7 @@ runuser -u irc-admin ./ircd
 
 # Configuring and starting Anope Services
 
-cp /tmp/anope-services.conf /opt/anope/conf/services.conf
-
-cd /opt/anope/bin
-runuser -u irc-admin ./anoperc start
+#cp /tmp/anope-services.conf /opt/anope/conf/services.conf
+#
+#cd /opt/anope/bin
+#runuser -u irc-admin ./anoperc start
